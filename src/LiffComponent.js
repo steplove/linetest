815cd2a6167449c9abeca994630bd544ccb59484
steplove~ <loveaumak@gmail.com>
2023-08-18 14:43:52 +0700

    g

--- src/LiffComponent.js
+++ src/LiffComponent.js
@@ -23,26 +23,22 @@ function LiffComponent() {
       <div>
         {profile ? (
           <div>
-            <img
-              src={profile.pictureUrl}
-              alt="Profile"
-              style={{ width: "100px", height: "100px" }} // เปลี่ยนขนาดตามที่คุณต้องการ
-            />
-            <p>Name: {profile.displayName}</p>
-            <p>Status: {liff.isLoggedIn() ? "Logged In" : "Logged Out"}</p>
+            {/* ...โค้ดอื่น ๆ... */}
           </div>
         ) : (
-          <>
+          <div>
             <p>Loading...</p>
-            <Spinner animation="grow" variant="primary" />
-            <Spinner animation="grow" variant="secondary" />
-            <Spinner animation="grow" variant="success" />
-            <Spinner animation="grow" variant="danger" />
-            <Spinner animation="grow" variant="warning" />
-            <Spinner animation="grow" variant="info" />
-            <Spinner animation="grow" variant="light" />
-            <Spinner animation="grow" variant="dark" />
-          </>
+            <div>
+              <Spinner animation="grow" variant="primary" />
+              <Spinner animation="grow" variant="secondary" />
+              <Spinner animation="grow" variant="success" />
+              <Spinner animation="grow" variant="danger" />
+              <Spinner animation="grow" variant="warning" />
+              <Spinner animation="grow" variant="info" />
+              <Spinner animation="grow" variant="light" />
+              <Spinner animation="grow" variant="dark" />
+            </div>
+          </div>
         )}
       </div>
     </Container>
